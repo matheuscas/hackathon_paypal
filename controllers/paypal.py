86a4b3469c19e8cefa17537d9af0eb614d61addd
matheuscas <matheus.mcas@gmail.com>
2014-01-31 16:22:04 -0200

    start

---
+++ controllers/paypal.py
@@ -0,0 +1,17 @@
+def index():
+	return dict()
+
+def set_express_checkout():
+	return dict()
+
+def get_express_checkout():
+	return dict()
+
+def do_express_checkout():
+	return dict()
+
+def ipn():
+	return dict()
+
+def refund():
+	return dict()				
